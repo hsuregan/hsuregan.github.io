f394d7b8be2e1b4eb386cdbea76d1148b0c44b7e
hsuregan <hsuregan@gmail.com>
2015-04-01 00:28:19 -0700

    more time wasted lol ... navbar sticks to the top when scrolled away

---
+++ JS/stick.js
@@ -0,0 +1,18 @@
+function sticky_relocate() {
+    var window_top = $(window).scrollTop();
+    var div_top = $('#sticky-anchor').offset().top;
+    if (window_top > div_top) {
+        $('#navbar').addClass('stick');
+        $('.navlink').addClass('stick_li');
+    } else {
+        $('#navbar').removeClass('stick');
+        $('.navlink').removeClass('stick_li');
+
+
+    }
+}
+
+$(function () {
+    $(window).scroll(sticky_relocate);
+    sticky_relocate();
+});
